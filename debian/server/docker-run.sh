f0cae1ff54ad6c8421f69069062965deb9824090
qiunet <qiunet@163.com>
2016-10-31 14:18:54 +0800

    add java images

--- debian/server/docker-run.sh
+++ debian/server/docker-run.sh
@@ -9,17 +9,19 @@
 
 if [ -n "${APP_NAME}" ];then
 	echo ${APP_NAME}
+	source /root/.bashrc
+
 	if [ ${APP_NAME} = "back" ] ;then
-		sed -i "s/code_front/code_back/g"  apache-tomcat-8.5.6/conf/server.xml
+		sed -i "s/code_front/code_back/g"  /root/apache-tomcat-8.5.6/conf/server.xml
 	elif [ ${APP_NAME} = "front" ];then
 		# 不需要修改
 		echo ""
 	else
 		echo "APP_NAME is error value"
-		exit 1
+		exit 1000
 	fi
 
-	/root/apache-tomcat-8.5.6/bin/startup.sh
+	/root/apache-tomcat-8.5.6/bin/catalina.sh run
 else
 	echo "APP_NAME is empty"
 	exit 1001
